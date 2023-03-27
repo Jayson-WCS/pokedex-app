8a6f2d72b7820b5950a5668b40a9629ccc456c33
Jayson_Delion <jayson-delion_student2023@wilder.school>
2023-03-27 16:57:22 +0200

    optionnal conditionning added

--- app-test/components/pokemonCard.jsx
+++ app-test/components/pokemonCard.jsx
@@ -1,12 +1,27 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const pokemon = pokemonList[1];
+
 function PokemonCard() {
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-      ></img>
-      <figcaption>Bulbasaur - フシギダネ</figcaption>
+      {pokemon.image ? (
+        <img src={pokemon.image} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
+
 export default PokemonCard;
