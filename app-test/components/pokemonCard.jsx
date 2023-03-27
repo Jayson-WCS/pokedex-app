c41e425dd193113a87518e6a7ed9b6658e7c76bf
Jayson_Delion <jayson-delion_student2023@wilder.school>
2023-03-27 23:17:26 +0200

    props

--- app-test/components/pokemonCard.jsx
+++ app-test/components/pokemonCard.jsx
@@ -1,21 +1,10 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+function PokemonCard(props) {
+  const { name, imgSrc } = props.pokemon;
 
-const pokemon = pokemonList[1];
-
-function PokemonCard() {
   return (
     <figure>
-      { pokemon.imgSrc ? ( <img src={pokemon.imgSrc} alt={pokemon.name} /> ) : ( <p>???</p> ) }
-      <figcaption>{ pokemon.name }</figcaption>
+      {imgSrc ? <img src={imgSrc} alt={name} /> : <p>???</p>}
+      <figcaption>{name}</figcaption>
     </figure>
   );
 }
--- app-test/src/App.jsx
+++ app-test/src/App.jsx
@@ -2,9 +2,22 @@ import "./App.css";
 import PokemonCard from "../components/pokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
     <div>
-      <PokemonCard />
+      {pokemonList.map((pokemon, i) => {
+        return <PokemonCard key={i} pokemon={pokemon} />;
+      })}
     </div>
   );
 }
